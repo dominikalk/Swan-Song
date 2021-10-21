258abb48abb8cfcdcddd155d835b1719dd925160
Dominik Alkhovik <alkhovikdg@cardiff.ac.uk>
2021-10-21 21:06:13 +0100

    add ground floor rooms

--- map.py
+++ map.py
@@ -22,18 +22,133 @@ room_<room name> = {
 
 from items import *
 
-room_demo = {
-    "name": "demo",
+room_lobby = {
+    "id": "lobby",
 
-    "description": """demo description""",
+    "name": "the lobby of the bank",
+
+    "description": """""",
     
     "items": [],
 
-    "exits": {},
+    "exits": {
+        'north': {
+            'room': 'tellers',
+            'time': 0,
+            'locked': False
+        },
+        'east': {
+            'room': 'office',
+            'time': 0,
+            'locked': False
+        } 
+    },
+}
+
+room_tellers = {
+    "id": "tellers",
+
+    "name": "the tellers of the bank",
+
+    "description": """""",
+    
+    "items": [],
+
+    "exits": {
+        'south': {
+            'room': 'lobby',
+            'time': 0,
+            'locked': False
+        },
+        'north': {
+            'room': 'security',
+            'time': 0,
+            'locked': False
+        } 
+    },
+}
+
+room_office = {
+    "id": "office",
+
+    "name": "the offices",
+
+    "description": """""",
+    
+    "items": [],
+
+    "exits": {
+        'south': {
+            'room': 'lobby',
+            'time': 0,
+            'locked': False
+        },
+        'west': {
+            'room': 'security',
+            'time': 0,
+            'locked': False
+        },
+        'north': {
+            'room': 'trading',
+            'time': 0,
+            'locked': False
+        }
+    },
+}
+
+room_security = {
+    "id": "security",
+
+    "name": "the security room",
+
+    "description": """""",
+    
+    "items": [],
+
+    "exits": {
+        'east': {
+            'room': 'office',
+            'time': 0,
+            'locked': False
+        },
+        'south': {
+            'room': 'tellers',
+            'time': 0,
+            'locked': False
+        },
+        'west': {
+            'room': 'stairs',
+            'time': 0,
+            'locked': False
+        },
+        'north': {
+            'room': 'trading',
+            'time': 0,
+            'locked': False
+        }
+    },
+}
+
+room_ceo = {
+    "id": "ceo",
+
+    "name": "the CEO's office",
+
+    "description": """""",
+    
+    "items": [],
+
+    "exits": {
+        'south': {
+            'room': 'trading',
+            'time': 0,
+            'locked': False
+        },
+    },
 }
 
 rooms = {
     #   VARIABLE NAME TEMPLATE
     # "<room name>": <VARIABLE OF ROOM>,
-    "demo": room_demo
+    "demo": room_lobby
 }
