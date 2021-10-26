362c466b9e2053c23b657cd910f601e2b27b27f3
Thomas White <whitet11@cardiff.ac.uk>
2021-10-26 10:40:16 +0100

    capitalise first letter of word

--- oja.py
+++
@@ -1,9 +0,0 @@
-string = "hello"
-string_list = string.split()
-string = ""
-print(string_list)
-for x in string_list:
-    if x.upper() != x:
-        x = x[0].upper()+ x[1:]
-    string = string + x + " "
-print(string)
