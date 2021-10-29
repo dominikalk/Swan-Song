a7fc554a82fcaa74b6a8cf9794ff583c23c41676
Dominik Alkhovik <dominik.alk@hotmail.com>
2021-10-29 01:58:28 +0100

    last changes

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ This helpers folder will contain a lot of the subroutines that we dont want in o
 as it would clutter them. We might make more helper files if this one gets too large.
 """
 
+import textwrap
 from map import rooms
 from ascii import *
 import os
@@ -70,7 +71,7 @@ def print_room_items(room):
     if(len(room['items']) == 0):
         return print('There is nothing in this room.\n')
 
-    print(f'There is {list_of_items(room["items"])} here.\n')
+    print(wrap(f'There is {list_of_items(room["items"])} here.') + '\n')
 
 
 def print_inventory_items(items):
@@ -85,7 +86,7 @@ def print_inventory_items(items):
     if(len(items) == 0):
         return print('There is nothing in your inventory.')
 
-    print(f'You have {list_of_items(items)}.')
+    print(wrap(f'You have {list_of_items(items)}.'))
 
 
 def print_room(room):
@@ -136,7 +137,7 @@ def print_room(room):
     print(room["name"].upper())
     print()
     # Display room description
-    print(room["description"])
+    print(wrap(room["description"]))
     print()
 
     # Display items in the room
@@ -148,7 +149,7 @@ def print_item(items):
     # Display items name
     print(f'You took {items["name"]}')
     # Display items description
-    print(items["description"])
+    print(wrap(items["description"]))
     value = items["value"]
     print(f"This item is worth: {format_price(value)}")
 
@@ -179,50 +180,12 @@ def print_exit(direction, leads_to):
     >>> print_exit("south", "MJ and Simon's room")
     GO SOUTH to MJ and Simon's room.
     """
-    print("GO " + direction.upper() + " to " + leads_to + ".")
+    is_enter = False
+    if direction == 'van' or direction == 'helicopter':
+        is_enter = True
 
-
-def print_menu(exits, room_items, inv_items):
-    """This function displays the menu of available actions to the player. The
-    argument exits is a dictionary of exits as exemplified in map.py. The
-    arguments room_items and inv_items are the items lying around in the room
-    and carried by the player respectively. The menu should, for each exit,
-    call the function print_exit() to print the information about each exit in
-    the appropriate format. The room into which an exit leads is obtained
-    using the function exit_leads_to(). Then, it should print a list of commands
-    related to items: for each item in the room print
-    "TAKE <ITEM ID> to take <item name>."
-    and for each item in the inventory print
-    "DROP <ITEM ID> to drop <item name>."
-    For example, the menu of actions available at the Reception may look like this:
-    You can:
-    GO EAST to your personal tutor's office.
-    GO WEST to the parking lot.
-    GO SOUTH to MJ and Simon's room.
-    TAKE BISCUITS to take a pack of biscuits.
-    TAKE HANDBOOK to take a student handbook.
-    DROP ID to drop your id card.
-    DROP LAPTOP to drop your laptop.
-    DROP MONEY to drop your money.
-    What do you want to do?
-    """
-    print("You can:")
-    # Iterate over available exits
-    for direction in exits:
-        # Print the exit name and where it leads to
-        print_exit(direction, exit_leads_to(exits, direction))
-
-    # Iterate over available take actions
-    for item in room_items:
-        # Print the exit name and where it leads to
-        print(f'TAKE {item["id"].upper()} to take {item["name"]}.')
-
-    # Iterate over available take actions
-    for item in inv_items:
-        # Print the exit name and where it leads to
-        print(f'DROP {item["id"].upper()} to drop {item["name"]}.')
-
-    print("What do you want to do?\n")
+    print(("ENTER " if is_enter else "GO ") + direction.upper() +
+          (" into " if is_enter else " to ") + leads_to + ".")
 
 
 def is_valid_exit(exits, chosen_exit):
@@ -357,6 +320,11 @@ def print_ending_score(item_list, win):
             'Play again to try to get more money and a better tier. Can you get to Tier 1?')
 
 
+def wrap(line):
+    broken = textwrap.wrap(line, 90, break_long_words=False)
+    return '\n'.join(broken)
+
+
 def display_score(item_list):
     total_value = calculate_value(item_list)
     tier = calculate_tier(total_value)
@@ -368,8 +336,7 @@ def display_score(item_list):
 
 def print_helpers():
     print(
-        '''
-The objective? Well that’s simple: steal as much as you can and escape before SWAT storm the bank. 
+        '''The objective? Well that’s simple: steal as much as you can and escape before SWAT storm the bank. 
 Do you have what it takes to get out?
 Possible commands:
 
